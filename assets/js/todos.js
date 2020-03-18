a72ab16766f6dd8c8d51e9d3cf793fdf6b90f925
Michał Sochacki <michalsochacki111@gmail.com>
2020-03-18 15:06:25 +0100

    Added toggling text input and refactor

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,18 +1,22 @@
+$('.toggleInputText').on('click', function() {
+    $('.textInput').toggle();
+});
+
 // Mark list item as completed
-$('ul').on('click', 'li', function() {
+$('.thingsToDo').on('click', 'li', function() {
     $(this).css('color', 'green');
 });
 
 // Delete list item
-$('ul').on('click', 'span', function() {
+$('.thingsToDo').on('click', 'span', function() {
     $(this).css('color', 'red');
 });
 
 // Add new list item
-$('input').keypress(function(event) {
+$('.textInput').keypress(function(event) {
     // If 'enter' is clicked
     if (event.which === 13) {
-        let inputValue = $(this).val();
-        $('ul').append('<li><span>X </span>' + inputValue + '</li>');
+        let toDoItem = $(this).val();
+        $('.thingsToDo').append('<li><span>X </span>' + toDoItem + '</li>');
     }
 });
