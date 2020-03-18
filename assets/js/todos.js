a12bdfd2ec223460774a2992a81e47b427f1a9a0
Michał Sochacki <michalsochacki111@gmail.com>
2020-03-18 09:04:20 +0100

    Initial commit

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,1 +1,17 @@
-alert("Hello world!");
+// Mark list item as completed
+$('li').click(function() {
+    $(this).css('color', 'green');
+});
+
+// Delete list item
+$('span').click(function() {
+    $(this).css('color', 'red');
+});
+
+// Add new list item
+$('input').keypress(function(event) {
+    // If 'enter' is clicked
+    if(event.which === 13) {
+        alert('You hit enter!');
+    }
+});
