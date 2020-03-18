159c6edb913e2bba90d95ecea41c76a56aed6708
Michał Sochacki <michalsochacki111@gmail.com>
2020-03-18 12:28:17 +0100

    Added functionality of adding new item into todo list

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,17 +1,18 @@
 // Mark list item as completed
-$('li').click(function() {
+$('ul').on('click', 'li', function() {
     $(this).css('color', 'green');
 });
 
 // Delete list item
-$('span').click(function() {
+$('ul').on('click', 'span', function() {
     $(this).css('color', 'red');
 });
 
 // Add new list item
 $('input').keypress(function(event) {
     // If 'enter' is clicked
-    if(event.which === 13) {
-        alert('You hit enter!');
+    if (event.which === 13) {
+        let inputValue = $(this).val();
+        $('ul').append('<li><span>X </span>' + inputValue + '</li>');
     }
 });
