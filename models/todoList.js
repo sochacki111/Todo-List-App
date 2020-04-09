dfcb6b082de0e2b81e22cb23bd47317adda56ed2
Michał Sochacki <michalsochacki111@gmail.com>
2020-04-09 12:39:47 +0200

    todoList model fix2

--- models/todoList.js
+++ models/todoList.js
@@ -6,14 +6,7 @@ const todoListSchema = new mongoose.Schema({
         {
             type: String
         }
-    ],
-    author: {
-        id: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'User',
-        },
-        username: String,
-    },
+    ]
 });
 
 const TodoList = mongoose.model('TodoList', todoListSchema);
