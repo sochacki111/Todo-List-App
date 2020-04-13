b8c1fc948fd2637df355bbeb1d371a9ee79bf5e0
sochacki111 <michalsochacki111@gmail.com>
2020-04-13 16:19:58 +0200

    Refactor

--- public/js/todos.js
+++ public/js/todos.js
@@ -1,74 +1,15 @@
-// // Toggle showing text input
-// $('.toggleInputText').on('click', function (event) {
-//     $('.textInput').slideToggle('fast');
-//     event.stopPropagation();
-// });
-
 // Mark list item as completed
 $('.list-group').on('click', '.list-group-item', function (event) {
     let todo_id = $(this).parent().data('todo_id');
     let task_id = $(this).data('task_id');
     // Create fetch patch
     fetch(`/todos/${todo_id}/tasks/${task_id}`, {
-        headers: { "Content-Type": "application/json; charset=utf-8" },
+        headers: {
+            "Content-Type": "application/json; charset=utf-8"
+        },
         method: 'PATCH',
-        // body: JSON.stringify({
-        //     isDone: true,
-        // })
     });
 
     $(this).toggleClass('done');
     event.stopPropagation();
 });
-
-// // Delete list item
-// $('.thingsTodo').on('click', '.trashCan', function (event) {
-//     $(this)
-//         .parent()
-//         .fadeOut(200, function () {
-//             $(this).remove();
-//         });
-//     event.stopPropagation();
-// });
-
-// // Add new list item
-// $('.textInput').keypress(function(event) {
-//     let toDoItem = $(this).val();
-//     // If 'enter' is clicked
-//     if (event.which === 13 && toDoItem !== '') {
-//         $('.thingsTodo').append(
-//             '<li class="todo"><span class="trashCan"><i class="fa fa-trash"></i></span>' +
-//                 toDoItem +
-//                 '</li>'
-//         );
-//         $(this).val('');
-//     }
-// });
-
-// Add new list item
-// $('.textInput').keypress(function(event) {
-//     let toDoItem = $(this).val();
-//     // If 'enter' is clicked
-//     if (event.which === 13 && toDoItem !== '') {
-//         fetch('/todos/addTask')
-//         .then
-//         $('.list-group').append(
-//             '<li class="list-group-item">Vestibulum at eros</li>'
-//         );
-//         $(this).val('');
-//     }
-// });
-
-// $('.listEntry').on('keypress', '.textInput', e => {
-//     let entry = $(this).val();
-//     if (event.which === 13 && entry !== '') {
-//         // $('.list-group').append(
-//         //     '<li class="list-group-item">Vestibulum at eros</li>'
-//         // );
-//         alert(1);
-//         console.log($(this));
-//         console.log($(this).parent());
-//         // $(this).submit();
-//         $(this).val('');
-//     }
-// });
