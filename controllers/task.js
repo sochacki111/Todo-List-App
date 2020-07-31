25af2fe87c5d23fd8971a932744d5b6e985d5a50
sochacki111 <michalsochacki111@gmail.com>
2020-07-31 09:06:08 +0200

    Refactor Task controller

--- controllers/task.js
+++ controllers/task.js
@@ -3,52 +3,46 @@ const Todo = require('../models/todo');
 const Task = require('../models/task');
 
 exports.createTask = async (req, res, next) => {
-  await Todo.findById(req.params.todo_id, (err, todo) => {
-    if (err) {
-      logger.error(err, 'task.controller.createTask.findById');
-      next(err);
-      redirect('/todos');
-    } else {
-      const task = {
-        text: req.body.task.text,
-        isDone: false
-      };
-      Task.create(task, (err, task) => {
-        if (err) {
-          next(err);
-          logger.error(err, 'task.controller.createTask.findById.create');
-        } else {
-          task.save();
-          logger.debug(`Created task: ${task}`);
+  try {
+    const todo = await Todo.findById(req.params.todo_id);
 
-          todo.tasks.push(task);
-          todo.save();
-          logger.debug(`Task: ${task} pushed to: ${todo}`);
+    // eslint-disable-next-line node/no-unsupported-features/es-syntax
+    const taskDeserialized = {
+      text: req.body.task.text,
+      isDone: false
+    };
 
-          res.redirect('/todos');
-          next();
-        }
-      });
-    }
-  });
+    const task = await Task.create(taskDeserialized);
+    task.save();
+    logger.debug(`Created task: ${task}`);
+
+    todo.tasks.push(task);
+    todo.save();
+    logger.debug(`Task: ${task} pushed to: ${todo}`);
+
+    next();
+  } catch (err) {
+    logger.error(err, 'task.controller.createTask.findById');
+    next(err);
+  } finally {
+    res.redirect('/todos');
+  }
 };
 
-exports.patchTask = async (req, res) => {
-  await Todo.findById(req.params.todo_id, (err, foundTodo) => {
-    if (err || !foundTodo) {
-      logger.error(err, 'task.controller.patchTask');
-      return res.redirect('/todos');
+exports.patchTask = async (req, res, next) => {
+  try {
+    const foundTodo = await Todo.findById(req.params.todo_id);
+    if (foundTodo && foundTodo.tasks.includes(req.params.task_id)) {
+      const foundTask = await Task.findById(req.params.task_id);
+      foundTask.isDone = !foundTask.isDone;
+      foundTask.save();
+      res.send('Task patched');
+      logger.debug(`Task: ${foundTodo} patched`);
+      next();
     }
-    if (foundTodo.tasks.includes(req.params.task_id)) {
-      Task.findById(req.params.task_id, (err, foundTask) => {
-        if (err) {
-          res.redirect('/todos');
-        } else {
-          foundTask.isDone = !foundTask.isDone;
-          foundTask.save();
-          res.send('Task patched');
-        }
-      });
-    }
-  });
+  } catch (err) {
+    logger.error(err, 'task.controller.patchTask');
+    next(err);
+    res.redirect('/todos');
+  }
 };
